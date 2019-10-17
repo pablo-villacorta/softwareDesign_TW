94548bebf7e44b9545c92de88b6f11aa684e7be4
pablo-villacorta <43268973+pablo-villacorta@users.noreply.github.com>
2019-10-17 09:12:02 +0200

    test commit

--- src/gui/Window.java
+++ src/gui/Window.java
@@ -34,29 +34,5 @@ public class Window extends JFrame {
 				}
 			}
 		} catch (Exception e) {}
-	}
-	
-	public static void main(String[] args) {
-		Window w = new Window();
-		showOnScreen(1, w);
-	}
-	
-	public static void showOnScreen(int screen, JFrame frame ) {
-	    GraphicsEnvironment ge = GraphicsEnvironment
-	        .getLocalGraphicsEnvironment();
-	    GraphicsDevice[] gs = ge.getScreenDevices();
-	    if( screen > -1 && screen < gs.length )
-	    {
-	        gs[screen].setFullScreenWindow( frame );
-	    }
-	    else if( gs.length > 0 )
-	    {
-	        gs[0].setFullScreenWindow( frame );
-	    }
-	    else
-	    {
-	        throw new RuntimeException( "No Screens Found" );
-	    }
-	}
-	
+	}	
 }
---
+++ src/program/Program.java
@@ -0,0 +1,36 @@
+package program;
+
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+
+import javax.swing.JFrame;
+
+import gui.Window;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Window w = new Window();
+		showOnScreen(1, w);
+	}
+	
+	// double screen related stuff
+	public static void showOnScreen(int screen, JFrame frame ) {
+	    GraphicsEnvironment ge = GraphicsEnvironment
+	        .getLocalGraphicsEnvironment();
+	    GraphicsDevice[] gs = ge.getScreenDevices();
+	    if( screen > -1 && screen < gs.length )
+	    {
+	        gs[screen].setFullScreenWindow( frame );
+	    }
+	    else if( gs.length > 0 )
+	    {
+	        gs[0].setFullScreenWindow( frame );
+	    }
+	    else
+	    {
+	        throw new RuntimeException( "No Screens Found" );
+	    }
+	}
+
+}
